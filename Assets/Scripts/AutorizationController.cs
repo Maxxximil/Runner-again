eb4aec42ede6f5158d0281d980faaa23744ad122
Maximil <borokm1@gmail.com>
2022-11-28 17:41:52 +0200

    Add Register and UI

---
+++ Assets/Scripts/AutorizationController.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class AutorizationController : MonoBehaviour
+{
+    [SerializeField] private GameObject _autorizationScreen;
+    [SerializeField] private GameObject _loginScreen;
+    [SerializeField] private GameObject _registrationScreen;
+
+    private bool _isopenAutorizationScreen = false;
+    private bool _isopenLoginScreen = false;
+    private bool _isopenRegistrationScreen = false;
+
+
+    private void Start()
+    {
+        _autorizationScreen.SetActive(false);
+        _loginScreen.SetActive(false);
+        _registrationScreen.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(_isopenAutorizationScreen || _isopenLoginScreen || _isopenRegistrationScreen)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    public void AutorizationScreen()
+    {
+        if (!_isopenAutorizationScreen)
+        {
+            _autorizationScreen.SetActive(true);
+            _isopenAutorizationScreen = true;
+        }
+        else
+        {
+            _autorizationScreen.SetActive(false);
+            _isopenAutorizationScreen = false;
+            _registrationScreen.SetActive(false);
+            _isopenRegistrationScreen = false;
+            _loginScreen.SetActive(false);
+            _isopenLoginScreen = false;
+        }
+    }
+
+    public void LoginScreen()
+    {
+        if (!_isopenLoginScreen)
+        {
+            if (_isopenRegistrationScreen)
+            {
+                _registrationScreen.SetActive(false);
+                _isopenRegistrationScreen = false;
+            }
+            _autorizationScreen.SetActive(false);
+            _isopenAutorizationScreen = false;
+            _loginScreen.SetActive(true);
+            _isopenLoginScreen = true;
+
+        }
+        else
+        {
+            _loginScreen.SetActive(false);
+            _isopenLoginScreen = false;
+            _autorizationScreen.SetActive(true);
+            _isopenAutorizationScreen = true;
+        }
+    }
+
+    public void RegistrationScreen()
+    {
+        if (!_isopenRegistrationScreen)
+        {
+            if (_isopenLoginScreen)
+            {
+                _loginScreen.SetActive(false);
+                _isopenLoginScreen = false;
+            }
+            _autorizationScreen.SetActive(false);
+            _isopenAutorizationScreen = false;
+            _registrationScreen.SetActive(true);
+            _isopenRegistrationScreen = true;
+
+        }
+        else
+        {
+            _registrationScreen.SetActive(false);
+            _isopenRegistrationScreen = false;
+            _autorizationScreen.SetActive(true);
+            _isopenAutorizationScreen = true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Firebase/Registration.cs
@@ -0,0 +1,165 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using Firebase;
+using Firebase.Auth;
+using TMPro;
+
+
+public class Registration : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField _inputUserName;
+    [SerializeField] private TMP_InputField _inputEmail;
+    [SerializeField] private TMP_InputField _inputPassword;
+    [SerializeField] private TMP_InputField _inputPasswordConfirm;
+    [SerializeField] private Text warningRegisterText;
+
+    public DependencyStatus dependencyStatus;
+    public FirebaseAuth auth;
+    public FirebaseUser user;
+
+    //public  
+
+    public UnityEvent success;
+
+    void Awake()
+    {
+        //Check that all of the necessary dependencies for Firebase are present on the system
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                //If they are avalible Initialize Firebase
+                InitializeFirebase();
+            }
+            else
+            {
+                Debug.LogError("Could not resolve all Firebase dependencies: " + dependencyStatus);
+            }
+        });
+    }
+
+    private void InitializeFirebase()
+    {
+        Debug.Log("Setting up Firebase Auth");
+        //Set the authentication instance object
+        //auth = FirebaseAuth.DefaultInstance;
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance; ;
+        auth.StateChanged += AuthStateChanged;
+        AuthStateChanged(this, null);
+
+    }
+    void AuthStateChanged(object sender, System.EventArgs eventArgs)
+    {
+        if (auth.CurrentUser != user)
+        {
+            bool signedIn = user != auth.CurrentUser && auth.CurrentUser != null;
+            if (!signedIn && user != null)
+            {
+                Debug.Log("Signed out " + user.UserId);
+            }
+            user = auth.CurrentUser;
+            if (signedIn)
+            {
+                Debug.Log("Signed in " + user.UserId);
+            }
+        }
+    }
+
+    void OnDestroy()
+    {
+        auth.StateChanged -= AuthStateChanged;
+        auth = null;
+    }
+
+
+    public void RegistrarionButton()
+    {
+        StartCoroutine(Register(_inputUserName.text, _inputEmail.text, _inputPassword.text));
+    }
+
+
+    private IEnumerator Register(string _username, string _email, string _password)
+    {
+        if (_username == "")
+        {
+            //If the username field is blank show a warning
+            warningRegisterText.text = "Missing Username";
+        }
+        else if (_inputPassword.text != _inputPasswordConfirm.text)
+        {
+            //If the password does not match show a warning
+            warningRegisterText.text = "Password Does Not Match!";
+        }
+        else
+        {
+            //Call the Firebase auth signin function passing the email and password
+            var RegisterTask = auth.CreateUserWithEmailAndPasswordAsync(_email, _password);
+            //Wait until the task completes
+            yield return new WaitUntil(predicate: () => RegisterTask.IsCompleted);
+
+            if (RegisterTask.Exception != null)
+            {
+                //If there are errors handle them
+                Debug.LogWarning(message: $"Failed to register task with {RegisterTask.Exception}");
+                FirebaseException firebaseEx = RegisterTask.Exception.GetBaseException() as FirebaseException;
+                AuthError errorCode = (AuthError)firebaseEx.ErrorCode;
+
+                string message = "Register Failed!";
+                switch (errorCode)
+                {
+                    case AuthError.MissingEmail:
+                        message = "Missing Email";
+                        break;
+                    case AuthError.MissingPassword:
+                        message = "Missing Password";
+                        break;
+                    case AuthError.WeakPassword:
+                        message = "Weak Password";
+                        break;
+                    case AuthError.EmailAlreadyInUse:
+                        message = "Email Already In Use";
+                        break;
+                }
+                warningRegisterText.text = message;
+            }
+            else
+            {
+                //User has now been created
+                //Now get the result
+                user = RegisterTask.Result;
+
+                if (user != null)
+                {
+                    //Create a user profile and set the username
+                    UserProfile profile = new UserProfile { DisplayName = _username };
+
+                    //Call the Firebase auth update user profile function passing the profile with the username
+                    var ProfileTask = user.UpdateUserProfileAsync(profile);
+                    //Wait until the task completes
+                    yield return new WaitUntil(predicate: () => ProfileTask.IsCompleted);
+
+                    if (ProfileTask.Exception != null)
+                    {
+                        //If there are errors handle them
+                        Debug.LogWarning(message: $"Failed to register task with {ProfileTask.Exception}");
+                        FirebaseException firebaseEx = ProfileTask.Exception.GetBaseException() as FirebaseException;
+                        AuthError errorCode = (AuthError)firebaseEx.ErrorCode;
+                        warningRegisterText.text = "Username Set Failed!";
+                    }
+                    else
+                    {
+                        //Username is now set
+                        //Now return to login screen
+                        success.Invoke();
+                        warningRegisterText.text = "";
+                    }
+                }
+            }
+        }
+    }
+
+}
